e47419c2816504d2db315b5c005aa0b32864b949
Vitor Rocha <deus.1928@gmail.com>
2025-06-17 13:43:33 -0300

    feat: enhance main function with database connection and logging

--- api/main.go
+++ api/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"crypt-link/config"
+	"crypt-link/database"
 	"crypt-link/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config.Load()
+	fmt.Println("Configuration loaded successfully")
+
+	err := database.Connect()
+	if err != nil {
+		panic("Failed to connect to database: " + err.Error())
+	}
+	fmt.Println("Database connected successfully")
 
 	r := router.Setup()
+	fmt.Println("Router setup completed successfully")
 
 	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
