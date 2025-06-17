a3415e4eb91e82ecdd7980269bba2d3d68f83415
Vitor Rocha <deus.1928@gmail.com>
2025-06-17 11:52:22 -0300

    docs: update Message struct documentation for clarity and completeness

--- api/model/message.go
+++ api/model/message.go
@@ -2,6 +2,9 @@ package model
 
 import "time"
 
+// Message represents an encrypted message with metadata including its unique identifier,
+// encrypted content, creation time, and expiration time. It is mapped to a BSON document
+// for storage in a MongoDB collection.
 type Message struct {
 	ID        string    `bson:"_id"`
 	Encrypted string    `bson:"encrypted"`
