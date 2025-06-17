e6fe2407eea71f9b57ec6163b9cf5bb9adb9c742
Vitor Rocha <deus.1928@gmail.com>
2025-06-17 13:43:47 -0300

    refactor: update Connect function to return error only and set global client and context

--- api/database/db.go
+++ api/database/db.go
@@ -3,17 +3,25 @@ package database
 import (
 	"context"
 	"crypt-link/config"
-	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Connect() (*mongo.Client, context.CancelFunc, error) {
+var Client *mongo.Client
+var Ctx = context.Background()
+
+func Connect() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.DatabaseUrl))
 
-	fmt.Println(config.DatabaseUrl)
-	return client, cancel, err
+	if err != nil {
+		return err
+	}
+
+	Client = client
+	Ctx = ctx
+	return err
 }
