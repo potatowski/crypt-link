937aa18f2ee826d5cb91b690df5304a057e2a968
Vitor Rocha <deus.1928@gmail.com>
2025-06-18 01:29:56 -0300

    feat: implement dark mode toggle and enhance styling for improved user experience

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -141,4 +141,28 @@ function closeModal() {
     alert.innerHTML = '';
 }
 
+const toggleBtn = document.getElementById('toggle-theme-btn');
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+        toggleBtn.textContent = 'Modo Claro';
+    } else {
+        localStorage.setItem('theme', 'light');
+        toggleBtn.textContent = 'Modo Escuro';
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        toggleBtn.textContent = 'Modo Claro';
+    } else {
+        toggleBtn.textContent = 'Modo Escuro';
+    }
+});
+
 window.onload = tryDecrypt;
